1cd6d3181daf49d20d6cac61d3655d40181d9606
sdabrowski <sdabrow2@gmail.com>
2017-04-20 14:30:59 -0500

    Introduction and Menu Complete

--- Library Card Catalog/Program.cs
+++ Library Card Catalog/Program.cs
@@ -17,19 +17,39 @@ namespace Library_Card_Catalog
             //bool is in Program class and allows changes to be made within different methods
             Program.IsRunning = true;
 
-            var path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "//Library Card Catalog.xml";
-           
+            Console.WriteLine("My Library Program \n(press enter)");
+            Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("To begin, we need to open your XML file.");
+            Console.WriteLine("Please input a filename that you would like to read.\nIf it does not exist, a new file will be created on your desktop with that name.");
+            Console.WriteLine("If you would like to create/open the default file, just press enter.");
+
+            //Read/Create a file from user input
+            string name;
+            name = Console.ReadLine();
+
+            //string path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "\\" + name + ".xml";
+
+            //Create a new file if no input from user
+            if (name == "")
+                name = "Library Card Catalog";
+
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "\\" + name + ".xml";
+
             //checks to see if "Library Card Catalog.xml exists or not
-            if(!System.IO.File.Exists(path))
+            if (!System.IO.File.Exists(path))
             {
                 //creates default file on users desktop and informs user of the file being created                 
                 System.IO.FileStream file = System.IO.File.Create(path);
+                Console.WriteLine("An XML file called {0} was created on your desktop.", name);
+                Console.ReadLine();
             }
-
-            Console.WriteLine("By default the Library Card Catalog will be on your desktop.");
-            Console.ReadLine(); // holds program for user
-
-
+            else
+            {
+                Console.WriteLine("You are going to open the file called {0}. Press Enter.", name);
+                Console.ReadLine();
+            }
 
             //do while loop that keeps running until user decides to exit
             do
